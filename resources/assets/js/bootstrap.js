47815c36ebe71c4e8ccb4248d278084eebfba49b
arifunlu <arifunlu@gmail.com>
2017-10-27 10:04:12 +0300

    changes

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+//window._ = require('lodash');
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -7,7 +7,7 @@ window._ = require('lodash');
  */
 
 try {
-    //window.$ = window.jQuery = require('jquery');
+    window.$ = window.jQuery = require('jquery');
 
     require('popper.js');
     require('bootstrap');
