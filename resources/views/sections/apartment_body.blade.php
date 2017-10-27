e96ecbf4b62a83243ac49b825d7f6a9a2a35ebf1
arifunlu <arifunlu@gmail.com>
2017-10-27 17:55:42 +0300

    apart change

--- resources/views/sections/apartment_body.blade.php
+++ resources/views/sections/apartment_body.blade.php
@@ -19,6 +19,20 @@
                 <div></div>
                 <canvas id="jPolygon" onmousedown="point_it(event)" data-imgsrc="{{$floor->floorPhoto ? $floor->floorPhoto->getImageUrl() : 'http://avrupark.com/images/kat-planlari/2+1-A-BLOK-2.jpg'}}"
                         width="960" height="540"></canvas>
+                <div style="display: inherit;position: absolute;right: 0;top: 37rem;">
+                    <div style="z-index: 100;">
+                        <img src="/img/medya.png" role="button" data-toggle="collapse" data-target="#medyaIcerik" onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
+                    </div>
+                    <div id="medyaIcerik" class="collapse">
+                        <div class="d-flex justify-content-between">
+                            <div id="slideVideo" role="button" class="d-flex flex-column align-items-center justify-content-between" style="color: #fff; width: 80px;"><img src="/img/cam.svg" style="width: 2.5rem;"/>Tanıtım Videosu</div>
+                            <div style="border-left: 1px solid #eee;"></div>
+                            <div id="slideDoc" role="button" class="d-flex flex-column align-items-center justify-content-between" style="color: #fff; width: 80px;"><img src="/img/doc.svg" style="width: 2rem;"/>Doküman</div>
+                            <div style="border-left: 1px solid #eee;"></div>
+                            <div id="slideFoto" role="button" class="d-flex flex-column align-items-center justify-content-between" style="color: #fff; width: 80px;"><img src="/img/img.svg" style="width: 2.5rem;"/>Foto Galeri</div>
+                        </div>
+                    </div>
+                </div>
                 <div class="d-flex flex-row justify-content-end" style="height: 91vh;">
                     @include('sections.right_bar')
                     <div id="rightIcerik" class="collapse">
@@ -105,3 +119,59 @@
         </div>
     </div>
 </main>
+
+
+
+    <div class="modal fade" id="slideModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <div id="slideControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <iframe class="d-block w-100" width="720" height="480" src="https://www.youtube.com/embed/M7lc1UVf-VE?controls=0&showinfo=0" frameborder="0"></iframe>
+            </div>
+            <div class="carousel-item">
+                <iframe class="d-block w-100" width="720" height="480" src="https://www.youtube.com/embed/M7lc1UVf-VE?controls=0&showinfo=0" frameborder="0"></iframe>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#slideControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#slideControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="slidePhoto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <div id="photoControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" width="720" height="480" src="http://192.81.215.163/uploads/project/phpRksG8G.jpg">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" width="720" height="480" src="http://192.81.215.163/uploads/project/phpRksG8G.jpg">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#photoControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#photoControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>
