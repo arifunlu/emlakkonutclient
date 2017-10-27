a997ccbb788dbebed1b95062da634533b6e7b5f3
arifunlu <arifunlu@gmail.com>
2017-10-27 14:19:02 +0300

    changes

--- resources/views/sections/body.blade.php
+++ resources/views/sections/body.blade.php
@@ -74,13 +74,13 @@
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img class="d-block w-100" src="..." alt="First slide">
+            <img class="d-block w-100" src="http://192.81.215.163/uploads/project/phpRksG8G.jpg" alt="First slide">
             </div>
             <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Second slide">
+            <img class="d-block w-100" src="http://192.81.215.163/uploads/project/phpRksG8G.jpg" alt="Second slide">
             </div>
             <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Third slide">
+                <iframe id="ytplayer" class="d-block w-100" type="text/html" width="720" height="405" src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&controls=0&showinfo=0" frameborder="0" allowfullscreen alt="Third slide">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
