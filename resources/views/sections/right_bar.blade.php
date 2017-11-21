7331c93e68d32ea44ee56928d34578bd74a6e44e
arifunlu <arifunlu@gmail.com>
2017-11-21 10:58:43 +0300

    changes

--- resources/views/sections/right_bar.blade.php
+++ resources/views/sections/right_bar.blade.php
@@ -1,9 +1,9 @@
-<div class="d-flex flex-column align-items-center" style="z-index: 99;">
+<div style="z-index: 99; width: 2rem;">
     <img src="/img/pay.png" style="margin-top: 3rem; display: none;" role="button" data-toggle="collapse"
          data-target="#rightIcerik" onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
-    <img src="/img/point.png" style="margin-top: 10rem;" onclick="toggleDraw(this);"
+    <img src="/img/draw.svg" style="margin-top: 10rem;" onclick="toggleDraw(this);"
          role="button"/>
-    <img src="/img/clear.png" onclick="clear_canvas();" role="button"
+    <img src="/img/clear.svg" onclick="clear_canvas();" role="button"
          onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
     <img src="/img/zoom_in.svg" onclick="updateZoom(0.2);" role="button"
          onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
