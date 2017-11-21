2d12e55183a5a4466e83c8cc4a7b85cba38ce5c2
arifunlu <arifunlu@gmail.com>
2017-11-21 11:26:55 +0300

    changes

--- resources/views/sections/right_bar.blade.php
+++ resources/views/sections/right_bar.blade.php
@@ -4,13 +4,13 @@
     <img src="/img/draw.svg" style="margin-top: 10rem;" onclick="toggleDraw(this);"
          role="button"/>
     <img src="/img/clear.svg" onclick="clear_canvas();" role="button"
-         onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
+         onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;" style="margin-top: 1rem;"/>
     <img src="/img/zoom_in.svg" onclick="updateZoom(0.2);" role="button"
-         onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
+         onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;" style="margin-top: 1rem;"/>
     <img src="/img/zoom_fit.svg" onclick="updateZoom(0);" role="button"
-         onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
+         onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;" style="margin-top: 1rem;"/>
     <img src="/img/zoom_out.svg" onclick="updateZoom(-0.2);" role="button"
-         onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
+         onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;" style="margin-top: 1rem;"/>
     <img src="/img/map.svg" onclick="window.location='{{ URL::route('map') }}'"
-         role="button" onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
+         role="button" onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;" style="margin-top: 1rem;"/>
 </div>
