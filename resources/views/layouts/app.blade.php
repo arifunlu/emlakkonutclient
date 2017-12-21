c72c9961041a278934c922b3246c97d8a8f7da8c
Arif Ünlü <arifunlu@gmail.com>
2017-12-22 00:32:50 +0300

    chj

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,21 +43,19 @@
                 });
 
                 $('#slideVideo').popover({
-                    trigger: 'focus',
+                    trigger: 'click',
                     html: true,
                     placement: 'top',
                     content: '<ul class="list-group-flush"><li class="list-group-item" data-toggle="modal" data-target="#slideModal">Cras justo odio</li><li class="list-group-item">Dapibus ac facilisis in</li><li class="list-group-item">Morbi leo risus</li><li class="list-group-item">Porta ac consectetur ac</li><li class="list-group-item">Vestibulum at eros</li></ul>'
                 });
 
                 $('#slideDoc').popover({
-                    trigger: 'focus',
                     html: true,
                     placement: 'top',
                     content: '<ul class="list-group-flush"><li class="list-group-item">Cras justo odio</li><li class="list-group-item">Dapibus ac facilisis in</li><li class="list-group-item">Morbi leo risus</li><li class="list-group-item">Porta ac consectetur ac</li><li class="list-group-item">Vestibulum at eros</li></ul>'
                 });
 
                 $('#slideFoto').popover({
-                    trigger: 'focus',
                     html: true,
                     placement: 'top',
                     content: '<ul class="list-group-flush"><li class="list-group-item" data-toggle="modal" data-target="#slidePhoto">Cras justo odio</li><li class="list-group-item">Dapibus ac facilisis in</li><li class="list-group-item">Morbi leo risus</li><li class="list-group-item">Porta ac consectetur ac</li><li class="list-group-item">Vestibulum at eros</li></ul>'
