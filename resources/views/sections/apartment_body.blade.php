d7253a1c064777fc2a22cbcfd5c9d155fa47ada2
Arif Ünlü <arifunlu@gmail.com>
2018-01-09 16:39:29 +0300

    fix

--- resources/views/sections/apartment_body.blade.php
+++ resources/views/sections/apartment_body.blade.php
@@ -38,7 +38,7 @@
                 <div></div>
                 <canvas id="jPolygon" onmousedown="point_it(event)" data-imgsrc="{{$floor->floorPhoto ? $floor->floorPhoto->getImageUrl() : 'http://avrupark.com/images/kat-planlari/2+1-A-BLOK-2.jpg'}}"
                         width="960" height="540"></canvas>
-                <div style="height: 91vh; padding-top: 12rem;">
+                <div style="height: 91vh; padding-top: 12rem; z-index: 99;">
                     <div style="z-index: 99; width: 2rem; margin-right: .1rem;">
                         <img src="/img/pay.png" style="margin-top: 3rem; display: none;" role="button" data-toggle="collapse"
                             data-target="#rightIcerik" onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;"/>
--- resources/views/sections/right_bar.blade.php
+++ resources/views/sections/right_bar.blade.php
@@ -1,4 +1,4 @@
-<div style="z-index: 99; width: 2rem; margin-right: .1rem;">
+<div style="width: 2rem; margin-right: .1rem;">
     <img src="/img/zoom_in.svg" onclick="updateZoom(0.2);" role="button"
          onmouseover="this.style.opacity=.6;" onmouseout="this.style.opacity=1;" style="margin-top: 1rem;"/>
     <img src="/img/zoom_fit.svg" onclick="updateZoom(0);" role="button"
