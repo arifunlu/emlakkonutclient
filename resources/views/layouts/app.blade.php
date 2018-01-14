045f35281a3589dffd2de8046eff12a64c6b1700
Arif Ünlü <arifunlu@gmail.com>
2018-01-14 21:36:11 +0300

    context menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,7 +86,7 @@
         }
         </style>
     </head>
-    <body oncontextmenu="return false">
+    <body>
         <button class="SlidePhotoBtnNext" onclick="sliderFullScreenNext()"></button>
         <button class="SlidePhotoBtnPrev" onclick="sliderFullScreenPrev()"></button>
         <div id="app">
--- resources/views/sections/apartment_body.blade.php
+++ resources/views/sections/apartment_body.blade.php
@@ -37,7 +37,7 @@
                 </div>
                 <div></div>
                 <canvas id="jPolygon" onmousedown="point_it(event)" data-imgsrc="{{$floor->floorPhoto ? $floor->floorPhoto->getImageUrl() : 'http://avrupark.com/images/kat-planlari/2+1-A-BLOK-2.jpg'}}"
-                        width="960" height="540"></canvas>
+                        width="960" height="540" oncontextmenu="return false"></canvas>
                 <div style="height: 91vh; padding-top: 12rem; z-index: 99;">
                     <div style="z-index: 99; width: 2rem; margin-right: .1rem;">
                         <img src="/img/pay.png" style="margin-top: 3rem; display: none;" role="button" data-toggle="collapse"
